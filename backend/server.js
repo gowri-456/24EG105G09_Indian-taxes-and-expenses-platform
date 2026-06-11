fdd4a19cacc3025a189dd30a9ec96fe4153d5a3d
gowri-456 <gowripriyachekka12345@gmail.com>
2026-06-11 18:04:15 +0530

    fix: import mongoose in server.js to resolve ReferenceError in health check

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import errorHandler from "./middleware/errorHandler.js";
-
+import mongoose from "mongoose";
 // Routes
 import authRoutes    from "./routes/authRoutes.js";
 import expenseRoutes from "./routes/expenseRoutes.js";
